84325d0bfb078542c8760ae728daed76ec1c86ab
echoyougf <youguifang2015>
2016-02-13 14:55:55 +0800

    update bulbshop

--- (2)bulbshop/myjs.js
+++ (2)bulbshop/myjs.js
@@ -1,1 +1,22 @@
-var oPic=document.getElementById();
+var oPic=document.getElementById("carousel").children;
+var oIndex=document.getElementById("indexs").children;
+var timer=null;
+function changeBanner(i){
+		oPic[i].className="show";
+		oIndex[i].className="change-indexColor";
+		for(j=0;j<oIndex.length;j++){
+			if(j!=i){
+				oPic[j].className="hide";
+				oIndex[j].className="index";
+			}
+		}
+	}
+
+autoChangeBanner();
+timer=setInterval("autoChangeBanner()","12000");
+function autoChangeBanner(){
+	setTimeout("changeBanner(0)","0");
+	setTimeout("changeBanner(1)","3000");
+	setTimeout("changeBanner(2)","6000");
+	setTimeout("changeBanner(3)","9000");
+}
